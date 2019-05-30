5da80e04607b746ce38a2583de738d7a66dcdb6b
Thomas <thomaschou861205@gmail.com>
2019-05-30 10:41:00 +0800

    b4 branch

--- hw4_1_105061110.py
+++ hw4_1_105061110.py
@@ -18,7 +18,6 @@ class LSTM():
     def __init__(self,
                 random_seed=42
                 ):
-        np.random.seed(random_seed)
         self.lr = 0.001                  # learning rate
         self.training_iters = 100000     # train step 上限
         self.batch_size = 128            
@@ -27,18 +26,27 @@ class LSTM():
         self.n_hidden_units = 128        # neurons in hidden layer
         self.n_classes = 10              # MNIST classes (0-9 digits)
 
-        g = tf.Graph()
-        with g.as_default():
-            tf.set_random_seed(random_seed)
-            self.build_model()
-            self.init_op = tf.global_variables_initializer()
-            # self.saver = tf.train.Saver()
-        self.sess = tf.Session(graph=g)
+        # g = tf.Graph()
+        # with g.as_default():
+        #     tf.set_random_seed(random_seed)
+        #     self.build_model()
+        #     self.init_op = tf.global_variables_initializer()
+        #     # self.saver = tf.train.Saver()
+        # gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
+        # self.sess = tf.Session(graph=g, config=tf.ConfigProto(gpu_options=gpu_options))
+
+
+        # **********att 2**************
+        tf.set_random_seed(random_seed)
+        self.build_model()
+        self.init_op = tf.global_variables_initializer()
+        # gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
+        # self.sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
 
 
     def build_model(self):
-        tf_x = tf.placeholder(tf.float32, shape=[None, self.n_steps, self.n_inputs], name='tf_x')
-        tf_y = tf.placeholder(tf.float32, shape=[None, self.n_classes], name='tf_y')
+        self.tf_x = tf.placeholder(tf.float32, shape=[None, self.n_steps, self.n_inputs], name='tf_x')
+        self.tf_y = tf.placeholder(tf.float32, shape=[None, self.n_classes], name='tf_y')
         weights = {
             # (28, 128)
             'in' : tf.Variable(tf.random_normal([self.n_inputs, self.n_hidden_units])),
@@ -54,14 +62,14 @@ class LSTM():
         }
 
         #  X: (128 batch, 28 steps, 28 inputs)
-        X = tf.reshape(tf_x, [-1, self.n_inputs]) # (128,*28, 28 inputs)
+        X = tf.reshape(self.tf_x, [-1, self.n_inputs]) # (128,*28, 28 inputs)
         X_in = tf.matmul(X, weights['in']) + biases['in'] # (128*28, 128 hidden)
         X_in = tf.reshape(X_in, [-1, self.n_steps, self.n_hidden_units]) # (128 batch, 28 steps, 128 hidden)
         
 
-        # tf.reset_default_graph()
-        lstm_cell = tf.contrib.rnn.BasicLSTMCell(
-            self.n_hidden_units, forget_bias=1.0, state_is_tuple=True)
+        # lstm_cell = tf.contrib.rnn.BasicLSTMCell(
+        #     self.n_hidden_units, forget_bias=1.0, state_is_tuple=True)
+        lstm_cell = tf.contrib.rnn.BasicLSTMCell(self.n_hidden_units)
         # lstm cell divided into two parts (c_state, m_state)
         # simple RNN has only m_state
         init_state = lstm_cell.zero_state(self.batch_size, dtype=tf.float32)
@@ -71,14 +79,14 @@ class LSTM():
 
         pred = tf.matmul(states[1], weights['out']) + biases['out']
         cost = tf.reduce_mean(
-            tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=tf_y),
+            tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=self.tf_y),
             name='cost'
         )
         train_op = tf.train.AdamOptimizer(self.lr).minimize(cost, name='train_op')
 
         correct_pred = tf.equal(
             tf.argmax(pred, 1),
-            tf.argmax(tf_y, 1),
+            tf.argmax(self.tf_y, 1),
             name='correct_preds'
         )
         accuracy = tf.reduce_mean(
@@ -94,15 +102,40 @@ class LSTM():
             batch_xs, batch_ys = mnist.train.next_batch(self.batch_size)
             batch_xs = batch_xs.reshape([self.batch_size, self.n_steps, self.n_inputs])
             self.sess.run('train_op', feed_dict={
-                'tf_x:0': batch_xs,
-                'tf_y:0': batch_ys
+                self.tf_x: batch_xs,
+                self.tf_y: batch_ys
             })
             if step % 20 == 0:
                 print(self.sess.run('accuracy', feed_dict={
-                    'tf_x:0': batch_xs,
-                    'tf_y:0': batch_ys}))
+                    self.tf_x: batch_xs,
+                    self.tf_y: batch_ys
+                    }))
             step += 1
 
+    def train_v2(self, mnist):
+        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
+        with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
+            # tf.initialize_all_variables() no long valid from
+            # 2017-03-02 if using tensorflow >= 0.12
+            if int((tf.__version__).split('.')[1]) < 12 and int((tf.__version__).split('.')[0]) < 1:
+                init = tf.initialize_all_variables()
+            else:
+                init = tf.global_variables_initializer()
+            sess.run(init)
+            step = 0
+            while step * self.batch_size < self.training_iters:
+                batch_xs, batch_ys = mnist.train.next_batch(self.batch_size)
+                batch_xs = batch_xs.reshape([self.batch_size, self.n_steps, self.n_inputs])
+                sess.run(['train_op'], feed_dict={
+                    self.tf_x: batch_xs,
+                    self.tf_y: batch_ys
+                })
+                if step % 20 == 0:
+                    print(sess.run(['accuracy'], feed_dict={
+                    self.tf_x: batch_xs,
+                    self.tf_y: batch_ys,
+                    }))
+                step += 1
 
     def predict(self):
         pass
@@ -116,5 +149,5 @@ if __name__ == "__main__":
     # 导入数据
     mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
     lstm = LSTM()
-    lstm.train(mnist)
+    lstm.train_v2(mnist)
     del lstm
