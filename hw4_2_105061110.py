97f67065b09f8c70e544e1c9d0c0d17735a1c1a7
Thomas <thomaschou861205@gmail.com>
2019-06-03 08:39:38 +0800

    'bout to cum

--- hw4_2_105061110.py
+++ hw4_2_105061110.py
@@ -5,6 +5,10 @@ import matplotlib.pyplot as plt
 import tensorflow.contrib.legacy_seq2seq as seq2seq
 from sklearn.model_selection import train_test_split
 
+# Good ref
+# Main structure: https://ppt.cc/feOA4x
+# https://zhuanlan.zhihu.com/p/27608348
+
 
 class en2fr():
 	def __init__(self):
@@ -20,12 +24,14 @@ class en2fr():
 
 		self.x = [line.split() for line in self.en_lines]
 		self.y = [line.split() for line in self.fr_lines]
-		self.epochs = 10
+		self.epochs = 20
 		self.batch_size = 1120
 		self.n_hidden = 256
 		self.embed_size = 16 # original dim: 228 & 357
 
 		self.word2int()
+		tf.reset_default_graph()
+		self.sess = tf.Session()
 		self.build_model()
 		self.saver = tf.train.Saver()
 		self.X_train, self.X_test, self.y_train, self.y_test = \
@@ -57,20 +63,22 @@ class en2fr():
 		self.x_seq_length = len(self.x[0])
 		self.y_seq_length = len(self.y[0])- 1
 
-	def batch_data(self, x, y, y_len, batch_size):
-	    shuffle = np.random.permutation(len(x))
-	    start = 0
-	    x = x[shuffle]
-	    y = y[shuffle]
-	    y_len = y_len[shuffle]
-	    while start + batch_size <= len(x):
-	        yield x[start:start+batch_size], y[start:start+batch_size], y_len[start:start+batch_size]
-	        start += batch_size
+	def batch_data(self, x, y=None, y_len=None, batch_size=32):
+		shuffle = np.random.permutation(len(x))
+		start = 0
+		x = x[shuffle]
+		if y is not None:
+			y = y[shuffle]
+			y_len = y_len[shuffle]
+
+		while start + batch_size <= len(x):
+			if y is not None:
+				yield x[start:start+batch_size], y[start:start+batch_size], y_len[start:start+batch_size]
+			else:
+				yield x[start:start+batch_size]
+			start += batch_size
 
 	def build_model(self):
-		tf.reset_default_graph()
-		self.sess = tf.Session()
-
 		inputs = tf.placeholder(tf.int32, (None, self.x_seq_length), 'inputs')
 		outputs = tf.placeholder(tf.int32, (None, None), 'outputs')
 		targets = tf.placeholder(tf.int32, (None, None), 'targets')
@@ -127,8 +135,8 @@ class en2fr():
 		    print('Epoch {:3} Loss: {:>6.4f} Accuracy: {:>6.4f} Epoch duration: {:>6.3f}s'.format(
 		    	epoch_i, batch_loss, accuracy, time.time() - start_time))
 
-		self.saver.save(self.sess, "model/{}.ckpt".format('seq2seq'))
-		print("model/{}.ckpt".format('seq2seq'))
+		self.saver.save(self.sess, 'model/seq2seq-{}.ckpt'.format(self.epochs))
+		print('Saved model/seq2seq-{}.ckpt'.format(self.epochs))
 
 		sample_output = True
 		if sample_output:
@@ -146,18 +154,66 @@ class en2fr():
 
 			accuracy = np.mean(dec_input[:,1:] == target_batch[:,1:])
 			print('Accuracy on test set is: {:>6.4f}'.format(accuracy))
-			num_preds = 10
-			source_chars = [[self.num2charX[l] for l in sent if self.num2charX[l]!="<PAD>"] for sent in source_batch[:num_preds]]
-			dest_chars = [[self.num2charY[l] for l in sent if self.num2charY[l]!="<PAD>" and self.num2charY[l]!="<GO>"] for sent in dec_input[:num_preds, 1:]]
-			for date_in, date_out in zip(source_chars, dest_chars):
-			    print(' '.join(date_in))
-			    print('=> '+' '.join(date_out))
-			    print()
+			self.translate()
+			# num_preds = 2
+			# source_chars = [[self.num2charX[l] for l in sent if self.num2charX[l]!="<PAD>"] for sent in source_batch[:num_preds]]
+			# dest_chars = [[self.num2charY[l] for l in sent if self.num2charY[l]!="<PAD>" and self.num2charY[l]!="<GO>"] for sent in dec_input[:num_preds, 1:]]
+			# for date_in, date_out in zip(source_chars, dest_chars):
+			#     print(' '.join(date_in))
+			#     print('=> '+' '.join(date_out))
+			#     print()
 
 	def translate(self):
-		pass
+		# self.saver.restore(self.sess, 'model/seq2seq-{}.ckpt'.format(30))
+		# print('Loaded model/seq2seq-{}.ckpt'.format(30))
+		
+		with open('test.txt', 'r', encoding='utf-8-sig') as f:
+			en_lines = f.readlines()
+		x = [line.split() for line in en_lines]
+		max_len_x = max([len(words) for words in x])
+		x = [[self.char2numX[word] for word in words] + [self.char2numX['<PAD>']]*(max_len_x - len(words)) for words in x]
+		
+		while len(x) < self.batch_size:
+			x.append(x[-1])
+		x = np.array(x)
+
+		# source_batch = next(self.batch_data(x=x, batch_size=self.batch_size))
+		source_batch = x
+
+		dec_input = np.zeros((len(source_batch), 1)) + self.char2numY['<GO>']
+		for i in range(self.y_seq_length):
+		    batch_logits = self.sess.run(self.logits,
+		                feed_dict = {
+		                 'inputs:0': source_batch,
+		                 'outputs:0': dec_input
+		                })
+		    prediction = batch_logits[:,-1].argmax(axis=-1)
+		    dec_input = np.hstack([dec_input, prediction[:,None]])
+		num_preds = 10
+		source_chars = [[self.num2charX[l] for l in sent if self.num2charX[l]!="<PAD>"] for sent in source_batch[:num_preds]]
+		source_ints = [[l for l in sent if self.num2charX[l]!="<PAD>"] for sent in source_batch[:num_preds]]
+		dest_chars = [[self.num2charY[l] for l in sent if self.num2charY[l]!="<PAD>" and self.num2charY[l]!="<GO>"] for sent in dec_input[:num_preds, 1:]]
+		dest_ints = [[l for l in sent if self.num2charY[l]!="<PAD>" and self.num2charY[l]!="<GO>"] for sent in dec_input[:num_preds, 1:]]
+
+		for en_char, en_int, fr_char, fr_int in zip(source_chars, source_ints, dest_chars, dest_ints):
+			print('Source (English)')
+			print('  Word Indices: ', end='')
+			print(en_int)
+			print('  English Words: ', end='')
+			print(en_char)
+			print()
+			print('Translation (French)')
+			print('  Word Indices: ', end='')
+			print([int(n) for n in fr_int])
+			print('  French Words: ', end='')
+			print(fr_char)
+			print()
 
 if __name__ == '__main__':
 	translator = en2fr()
 	translator.train()
+
+	# translator.translate()
+
+
 
